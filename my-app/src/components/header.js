5dae6f97561d842bba90c392f4d36a7438cada75
nonfungiblehayor <nonfungiblehayor.eth@gmail.com>
2022-11-15 16:46:23 +0100

    style: style mobile breakpoint

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../Logo.svg'
+import menu from '../menu.png'
 
 function Header() {
+    const [closeMenu,openMenu] = useState(false)
+
+    const showMenu = function() {
+        openMenu(!closeMenu)
+    }
     return <header>
         <img src={Logo} alt='metabnb'></img>
+        <img src={menu} alt='menu-bar-icon' className='menu' onClick={showMenu}></img>
         <div>
             <a href='./app.js'>Home</a>
             <a href='./app.js'>Place to stay</a>
             <a href='./app.js'>NFTs</a>
             <a href='./app.js'>Community</a>
+            <button type='submit' className='menu_btn'>connect wallet</button>
         </div>
-        <button type='submit'>connect wallet</button>
+        <button type='submit' className='connect'>connect wallet</button>
     </header>
 }
 
