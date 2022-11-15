e8d9fceb34cc1f927ee12f2d521a72ec79c654a4
nonfungiblehayor <nonfungiblehayor.eth@gmail.com>
2022-11-15 19:55:23 +0100

    feat: render mobile menu based on state

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -1,23 +1,29 @@
 import React, { useState } from 'react'
 import Logo from '../Logo.svg'
 import menu from '../menu.png'
+import cancel from '../cancel.png'
 
 function Header() {
-    const [closeMenu,openMenu] = useState(false)
-
-    const showMenu = function() {
-        openMenu(!closeMenu)
-    }
+    const [openMenu,closeMenu] = useState(false)
+    
+   const showMenu = function() {
+       closeMenu(!openMenu)
+   }
+   const hideMenu = function() {
+       closeMenu(!openMenu)
+   }
     return <header>
+        {/* {openMenu ? '' : } */}
         <img src={Logo} alt='metabnb'></img>
         <img src={menu} alt='menu-bar-icon' className='menu' onClick={showMenu}></img>
-        <div>
+        {openMenu ? <div>
+            <img src={cancel} alt='cancel' onClick={hideMenu}></img>
             <a href='./app.js'>Home</a>
             <a href='./app.js'>Place to stay</a>
             <a href='./app.js'>NFTs</a>
             <a href='./app.js'>Community</a>
             <button type='submit' className='menu_btn'>connect wallet</button>
-        </div>
+        </div> : '' }
         <button type='submit' className='connect'>connect wallet</button>
     </header>
 }
